7a07a516d47f5c7e9455fe8b7cd2f95ba5110300
RohithSagar04 <rohithsagar04@github.com>
2026-03-06 11:22:25 +0530

    first commit

---
+++ Hi.java
@@ -0,0 +1,5 @@
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("hello this is rohith,working on my first java programming ");
+    }
+}
---
+++ reverse.py
@@ -0,0 +1,7 @@
+def reverse_string(s):
+    return s[::-1]   # slicing technique
+
+# Example usage
+text = input("Enter a string: ")
+reversed_text = reverse_string(text)
+print("Reversed string:", reversed_text)
